9e5632b1b5021c267b2b786dc96359404fe4abff
uyruyr777 <uyruyr777@gmail.com>
2026-08-11 15:45:00 +0300

    Update don.sh

--- don.sh
+++ don.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 set -e
 
+TMP=$(mktemp -d)
+curl -fsSL https://github.com/uyruyr777/kli-s-s/archive/refs/heads/main.tar.gz | tar -xz -C "$TMP"
+
 rm -rf "$HOME/.kli-s-s"
 mkdir -p "$HOME/.kli-s-s"
 
-curl -fsSL https://github.com/uyruyr777/kli-s-s/archive/refs/heads/main.tar.gz | tar -xz -C "$HOME/.kli-s-s" --strip-components=2 "kli-s-s-main/l"
+shopt -s dotglob
+mv "$TMP"/*/l/* "$HOME/.kli-s-s"/
+
+rm -rf "$TMP"
 
 cd "$HOME/.kli-s-s"
 chmod +x is.sh
