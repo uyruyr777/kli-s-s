e5521d384ecdb85474186e0ec837bd7caf022bb3
uyruyr777 <uyruyr777@gmail.com>
2026-08-11 15:13:01 +0300

    Update don.sh

--- don.sh
+++ don.sh
@@ -3,8 +3,8 @@ set -e
 
 rm -rf "$HOME/.kli-s-s"
 mkdir -p "$HOME/.kli-s-s"
-curl -fsSL https://github.com/uyruyr777/kli-s-s/archive/refs/heads/main.tar.gz | tar -xz -C "$HOME/.kli-s-s" --strip-components=1
+curl -fsSL https://github.com/uyruyr777/kli-s-s/archive/refs/heads/main/l.tar.gz | tar -xz -C "$HOME/.kli-s-s" --strip-components=1
 
-cd "$HOME/.kli-s-s/l"
+cd "$HOME/.kli-s-s"
 chmod +x is.sh
 ./is.sh
